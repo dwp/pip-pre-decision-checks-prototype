726c0f471744389019f94990919906aa7bf8bc6f
PaulCoffield-DWP <146944039+PaulCoffield-DWP@users.noreply.github.com>
2024-05-28 17:25:50 +0100

    Merge pull request #4 from dwp/v0
    WIP - v1 screens, more content
    
    GitOrigin-RevId: fbbe205af9c5050ccc0496e423b144dd2a9ebd86

---
+++ app/data/questions-data.js.js
@@ -0,0 +1,39 @@
+module.exports = {
+
+  "question-1" : {
+    id: 1,
+    'question-text':'',
+    'help-text':'',
+    'question-group':'lay-conditions',
+    'response': null
+
+  },
+  "question-1" : {
+    id: 2,
+    'question-text':'',
+    'help-text':'',
+    'question-group':'lay-conditions',
+    'response': null
+    
+
+  },
+  "question-3" : {
+    id: 3,
+    'question-text':'',
+    'help-text':'',
+    'question-group':'lay-conditions',
+    'response': null
+    
+
+  },
+
+  "question-4" : {
+    id: 4,
+    'question-text':'',
+    'help-text':'',
+    'question-group':'lay-conditions',
+    'response': null
+
+  },
+
+}
