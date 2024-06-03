3d83e01c21038a3ba2df9de7a281d12a96d6679c
Paul Coffield <paul.coffield@engineering.digital.dwp.gov.uk>
2024-06-03 10:42:18 +0100

    Completed lay conditions and payability checks

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,75 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+
+// LAY CONDITIONS
+
+router.post('/v1/lay-conditions/lay-q-1', (req, res) => {
+
+ if (req.query.returnURL){
+    res.redirect(req.query.returnURL);
+  } else {
+    res.redirect('lay-q-2');
+  }
+
+});
+
+router.post('/v1/lay-conditions/lay-q-2', (req, res) => {
+
+  if (req.query.returnURL) {
+    res.redirect('lay-conditions-cya');
+  } else {
+    res.redirect('lay-q-3');
+  }
+
+});
+
+router.post('/v1/lay-conditions/lay-q-3', (req, res) => {
+
+  if (req.query.returnURL) {
+    res.redirect(req.query.returnURL);
+  } else {
+    res.redirect('lay-q-4');
+  }
+
+});
+
+router.post('/v1/lay-conditions/lay-q-4', (req, res) => {
+
+  if (req.query.returnURL) {
+    res.redirect('lay-conditions-cya');
+  } else {
+    res.redirect('lay-conditions-cya');
+  }
+
+});
+
+
+// PAYABILITY CHECKS
+
+router.post('/v1/payability-checks/pay-q-1', (req, res) => {
+
+  if (req.query.returnURL){
+     res.redirect(req.query.returnURL);
+   } else {
+     res.redirect('pay-q-2');
+   }
+ });
+
+ router.post('/v1/payability-checks/pay-q-2', (req, res) => {
+
+  if (req.query.returnURL){
+     res.redirect(req.query.returnURL);
+   } else {
+     res.redirect('pay-q-3');
+   }
+ });
+
+ router.post('/v1/payability-checks/pay-q-3', (req, res) => {
+
+  if (req.query.returnURL){
+     res.redirect(req.query.returnURL);
+   } else {
+     res.redirect('payability-checks-cya');
+   }
+ });
